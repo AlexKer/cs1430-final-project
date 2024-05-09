85bed08af9eb88348737e7006eac7c7f8961d6d7
Seik Oh <seikoh@naver.com>
2024-05-09 17:02:33 -0400

    VGG model refactorized

--- code/model.py
+++ code/model.py
@@ -1,74 +1,69 @@
 import torch
 import torch.nn as nn
+import torchvision.models as models
 import torch.optim as optim
+from emotion_dataloader import get_data_dl
 
 # reference from HW5
 class VGGModel(nn.Module):
-    def __init__(self, learning_rate):
+    def __init__(self, batch_size:int, device='cuda', lr=0.09) -> None:
         super(VGGModel, self).__init__()
-
-        self.vgg16 = nn.Sequential(
+        self.batch_size = batch_size
+        self.device = device
+        self.features = nn.Sequential(
             # Block 1
-            nn.Conv2d(1, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             # Block 2
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             # Block 3
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             # Block 4
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             # Block 5
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        
-        # 7 classifications
-        self.head = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(512, 512),  # Adjusted from 512 * 7 * 7 to 512 due to shape errors
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 7),
-            # nn.Softmax(dim=1)
-        )
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        ).to(device)
 
-        # Optimizer
-        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512 * 7 * 7, 4096), #2048?
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(4096, 7),
+        ).to(device)
+
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
 
     def forward(self, x):
-        """ Passes the image through the network. """
-        x = self.vgg16(x)
-        # print("Output size after vgg16:", x.shape)  # check output size
-        x = self.head(x)
+        x = self.features(x)
+        x = self.classifier(x)
         return x
-
-    @staticmethod
-    def loss_fn(predictions, labels):
-        """ Loss function for model. """
-        return nn.CrossEntropyLoss()(predictions, labels.long())
