8e2c6b18a96576d70541bef6171cfa50019b661d
Seik Oh <seikoh@naver.com>
2024-05-09 23:17:12 -0400

    edit sh file

--- code/run_job.sh
+++ code/run_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name="train_model"
+#SBATCH --job-name="vgg_model"
 #SBATCH --partition=gpus
 #SBATCH --mem=120G
 #SBATCH --cpus-per-task=4
@@ -8,7 +8,7 @@
 #SBATCH -e job_%x_%j.e
 #SBATCH -o job_%x_%j.o
 #SBATCH --nodes=1
-#SBATCH --exclude=gpu[1601-1605,1701-1708,1801-1802,1901-1904,1905-1906,2002-2003]
+#SBATCH --exclude=gpu[1601-1605,1701-1708,1801-1802,1905-1906]
 #SBATCH --gpus-per-node=1
 
 source activate ~/miniconda3/envs/CVfinal_env
